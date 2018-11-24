c05441a3a01280d48614081bfb22284c1202f1ff
hetykai <3378432+hetykai@users.noreply.github.com>
2018-11-24 10:55:45 +0800

    Update build.sh
    修改镜像名

--- build.sh
+++ build.sh
@@ -4,10 +4,9 @@ set -eu
 
 gitea_version='1.6.0'
 gitea_checksum='0da8bc9ec0e006a1a3209a46e255236590e4eb7e'
-image_name='orzech/gitea'
+image_name='hety/gitea'
 
 docker build \
-  --disable-content-trust=false \
   --build-arg GITEA_VERSION="$gitea_version" \
   --build-arg GITEA_CHECKSUM="$gitea_checksum" \
   -t "${IMAGE_NAME:-${image_name}}" \
