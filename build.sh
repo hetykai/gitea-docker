667141329d4204452f651c6fa3a803589807fe3f
hetykai <3378432+hetykai@users.noreply.github.com>
2018-11-24 23:01:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@
 set -eu
 
 gitea_version=$(docker run --rm busybox wget -q https://github.com/go-gitea/gitea/releases/latest --no-check-certificate -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}' | tail -n 1)
+repoName='hety/gitea'
 
 docker build \
   --build-arg GITEA_VERSION="$gitea_version" \
-  -t "$DOCKER_REPO:latest" \
-  -t "$DOCKER_REPO:$gitea_version" \
+  -t "$repoName:$gitea_version" \
   .
 
-docker push $DOCKER_REPO:$gitea_version
-docker push $DOCKER_REPO:latest
+docker push $repoName:$gitea_version
