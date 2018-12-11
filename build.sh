9c6160a4bf43f515ac4aa87c6b6eb6b263558a87
hetykai <3378432+hetykai@users.noreply.github.com>
2018-12-11 20:01:25 +0800

    Update build.sh
    测试

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu
 
 gitea_version=$(docker run --rm busybox wget -q https://github.com/go-gitea/gitea/releases/latest --no-check-certificate -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}' | tail -n 1)
 repoName='hety/gitea'
-
+echo "######################### Test auto build latest gitea docker image #########################"
 docker build \
   --build-arg GITEA_VERSION="$gitea_version" \
   -t "$repoName:latest" \
