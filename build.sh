3393ec6fe03e282398d949fad37567c10592dd13
Piotr Orzechowski <piotr@orzechowski.tech>
2019-01-22 01:24:13 +0100

    Bump gitea version to 1.6.4

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-gitea_version='1.6.3'
-gitea_checksum='30560b0f9b750cb96497ce110577c4293fc863ca'
+gitea_version='1.6.4'
+gitea_checksum='eb0ee6bf584b530f5cfc23838112317560a5d925'
 image_name='orzech/gitea'
 
 docker build \
