d05b7986881e75d9a972dc550c19722b8100d119
Piotr Orzechowski <piotr@orzechowski.tech>
2019-02-01 03:38:54 +0100

    Bump gitea version to 1.7.1

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-gitea_version='1.7.0'
-gitea_checksum='3fa49f3780ad3d79348f872f2523f57113d47ade'
+gitea_version='1.7.1'
+gitea_checksum='dfad569e40edf86346a10d11bc68c98c12bca30d'
 image_name='orzech/gitea'
 
 docker build \
