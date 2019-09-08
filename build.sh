cd8e288734ea0ca693f42a22b384380f6ba6cdb2
Piotr Orzechowski <piotr@orzechowski.tech>
2019-09-08 22:24:48 +0200

    Bump gitea version to 1.9.3

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-gitea_version='1.9.2'
-gitea_checksum='30dbddcc4de73c1e359f863108c09dabab3ef4ef'
+gitea_version='1.9.3'
+gitea_checksum='2f715713052b6e3dc1f912b333816da414e99743'
 image_name='orzech/gitea'
 source='https://orzechowski.tech/git/orzech/gitea-docker'
 
